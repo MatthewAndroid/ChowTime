e5904714e1903845cb30e824505e4b4d872ea514
MatthewAndroid <matthewmaymangithub@gmail.com>
2025-04-03 01:08:19 +0200

    made getMealType function

--- app/src/main/java/com/example/chowtime/MainActivity.kt
+++ app/src/main/java/com/example/chowtime/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chowtime.ui.theme.ChowTimeTheme
 
@@ -19,29 +15,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChowTimeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MealSuggestionScreen()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MealSuggestionScreen() {
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChowTimeTheme {
-        Greeting("Android")
+
+
+fun getMealType(hour: Int): String {
+    val morningMeals = arrayOf("Pancakes", "Omelette", "Smoothie")
+    val midMorningMeals = arrayOf("Fruit Salad", "Yogurt", "Granola Bar")
+    val afternoonMeals = arrayOf("Sandwich", "Salad", "Soup")
+    val midAfternoonMeals = arrayOf("Cookies", "Muffin", "Tea")
+    val dinnerMeals = arrayOf("Steak", "Pasta", "Grilled Chicken")
+
+    if(hour < 9 && hour > 5){
+        return morningMeals.random()
+    } else if(hour < 11 && hour > 9){
+        return midMorningMeals.random()
+    } else if(hour < 14 && hour > 12){
+        return afternoonMeals.random()
+    } else if(hour < 17 && hour > 15){
+        return midAfternoonMeals.random()
+    } else if(hour < 21 && hour > 18){
+        return dinnerMeals.random()
+    } else {
+        return "No meal"
+
     }
 }
