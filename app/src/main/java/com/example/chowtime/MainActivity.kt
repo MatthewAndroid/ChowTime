b56ce696fe632dcf4ccba41ec9fc8d214b2cd4c9
MatthewAndroid <matthewmaymangithub@gmail.com>
2025-04-03 01:09:37 +0200

    made TimeSelectorButton composable

--- app/src/main/java/com/example/chowtime/MainActivity.kt
+++ app/src/main/java/com/example/chowtime/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.chowtime
 
+import android.icu.util.Calendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chowtime.ui.theme.ChowTimeTheme
 
@@ -27,7 +35,37 @@ fun MealSuggestionScreen() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimeSelectorButton(
+    onConfirm: (String) -> Unit = {},
+    onDismiss: () -> Unit = {}
+) {
+    val currentTime = Calendar.getInstance()
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
 
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        TimePicker(
+            state = timePickerState,
+        )
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = onDismiss) {
+                Text("Close")
+            }
+            Button(onClick = {
+                val selectedHour = timePickerState.hour
+                val meal = getMealType(selectedHour)
+                onConfirm(meal)
+            }) {
+                Text("Select")
+            }
+        }
+    }
+}
 
 fun getMealType(hour: Int): String {
     val morningMeals = arrayOf("Pancakes", "Omelette", "Smoothie")
